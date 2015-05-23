3bcbf3d1188c9ffc176f531bc07a65b947230930
Lubos Lenco <lubos.lenco@gmail.com>
2015-05-24 00:25:19 +0200

    Get velocity

--- Sources/haxebullet/Bullet.hx
+++ Sources/haxebullet/Bullet.hx
@@ -225,7 +225,9 @@ extern class BtRigidBody extends BtCollisionObject {
 	public function getCenterOfMassTransform():BtTransform; 
 	public function setCenterOfMassTransform(trans:BtTransform):Void;
 	//public function setGravity(acceleration:BtVector3):Void; // not available in ammo
+	public function getLinearVelocity():BtVector3;
 	public function setLinearVelocity(lin_vel:BtVector3):Void;
+	public function getAngularVelocity():BtVector3;
 	public function setAngularVelocity(ang_vel:BtVector3):Void;
 	public function setLinearFactor(linearFactor:BtVector3):Void;
 	public function setAngularFactor(angFac:BtVector3):Void;
