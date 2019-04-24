8ca55f5255c9063e36b7f2344c2780488c742b61
Zicklag <zicklag@katharostech.com>
2019-04-24 10:33:57 -0500

    Remove Khabind.json And Comment Options In Khafile.js

--- khafile.js
+++ khafile.js
@@ -1,11 +1,21 @@
 let project = new Project('haxebullet');
 
 project.khabind({
+    // forceCache makes sure that the bindings will not be regenerated ( so
+    // that users don't end up needing to install emscripten if they
+    // accidentally touch this file ). Must be disabled to update bindings.
+    forceCache: true,
+    // The path to the IDL file containing the bindings
     idlFile: "bullet.idl",
+    // The name of the C/C++ library
     nativeLib: "bullet",
+    // The path to the C/C++ source files
     sourcesDir: "bullet",
+    // Optionally strip a prefix from the class names
     chopPrefix: "bt",
+    // Attempt to auto garbage collect references ( only for HashLink )
     autoGC: false,
+    // Headers to include
     includes: [
         "<btBulletDynamicsCommon.h>",
         "<BulletSoftBody/btSoftBody.h>",
@@ -19,7 +29,9 @@ project.khabind({
         "<BulletCollision/Gimpact/btGImpactShape.h>",
         "<BulletCollision/Gimpact/btGImpactCollisionAlgorithm.h>"
     ],
+    // Optimization level for JavaScript compiler ( either 1, 2, s, z, or 3 )
     emccOptimizationLevel: "3",
+    // Arguments for the JavaScript compiler
     emccArgs: [
         "--llvm-lto 1",
         "-s NO_EXIT_RUNTIME=1",
